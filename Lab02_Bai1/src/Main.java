9bc24c6ebfb6c0e7bebe61c9772a23063113ee0f
dohaminhduc <167590808+dohaminhduc@users.noreply.github.com>
2024-05-06 09:43:22 +0700

    Lab02

---
+++ Lab02_Bai1/src/Main.java
@@ -0,0 +1,24 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    /*Complex com1 = new Complex();
+    Complex com2 = new Complex();
+    com1.getNum();
+    com2.getNum();
+    com1.AddNum(com2).print();
+    com1.SubsNum(com2).print();
+    com1.MultiNum(com2).print();
+    com1.DivNum(com2).print();*/
+       /* Company company = new Company(10);
+        company.inputEmployees();
+        company.printSalarySheet();
+        company.printEmployeesBySalary();
+        company.highestSalaryEmployee();*/
+        Bank bank = new Bank();
+        bank.addAccount();
+        bank.TransferMoney();
+        bank.DepositAccount();
+        bank.Print();
+        }
+    }
